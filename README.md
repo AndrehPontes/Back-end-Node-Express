e8ce907fa6e1a9760c343a622f76678834f73762
Andre Pontes <andreh.webmaster@gmail.com>
2022-02-17 23:41:13 -0400

    SGI-02: Method GEt request

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Back-end-Node-Express
 Back-end utilizando o Nodejs, Nodemon e Expressjs.
+
+Tecnlogias:
+
+- Node
+- Express
+- Nodemon
+- REST CLIENT
+
+Rotas:
+
+1 - Cadastrar Produto
+2 - Buscar todos os produtos cadastrados
+3 - Buscar Produto por ID
+4 - Rota para fazer delete
+5 - Rota alteração
--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express =  require("express");
+const {randomUUID} =  require("crypto");
 
 const app = express();
+
+app.use(express.json());
+
+const products = [];
+
+/* ROTAS BÁSICAS
 
 app.get("/", (request, response) => {
     return response.json({
@@ -18,6 +25,50 @@ app.get("/produto", (request, response) => {
     return response.json({
         message: "Rota de produtos.",
     });
+}); 
+
+FIM ROTAS BÁSICAS */
+
+
+/** 
+ * POST   => Inserir um dado
+ * GET    => Buscar ou inserir mais de um dado
+ * PUT    => Alterar um dado
+ * DELETE => Remover um dado 
+ * 
+ * /
+ 
+/**
+ * Body   => Sempre que eu quiser enviar dados para aplicação;
+ * Params => /products/1546584154484
+ * Query  => /products?id=487878415498485value=45984548748
+ */
+
+app.post("/products", (request, response) => {
+    //nome e preço => name and price
+
+    const { name, price } = request.body    //desestruração
+
+    const product = {
+     name,
+     price,
+     id: randomUUID(),   
+    }
+    
+    products.push(product);
+
+   return response.json(product);
+});
+
+
+app.get("/products", (request, response) => {
+    return response.json(products);
+});
+
+app.get("/products/:id", (request, response) => {
+    const { id } = request.params;
+    const product = products.find((product) => product.id == id); 
+    return response.json(product);
 });
 
 
